f405b4197abee5fbd2a3d7d291cb0f3ce5e490e4
alexsb95 <alexsb95@gmail.com>
2016-06-16 21:04:14 -0600

    Dto
    Dto a utilizar

---
+++ Codigo/Dto.java
@@ -0,0 +1,47 @@
+
+package Utilidades.Dto;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+
+public class Dto {
+
+    private Map<String, Object> diccionario;    
+    public Dto(){
+        diccionario = new TreeMap<>();
+    }
+    public boolean agregarElemento (String pLlave, Object pValor){
+        if(!diccionario.containsKey(pLlave.toLowerCase())){
+            diccionario.put(pLlave.toLowerCase(), pValor);
+            return true;
+        }
+        return false;
+    }
+    
+    public Object buscarLlave(String pLlave){
+        Object valor = "";
+        
+        if(diccionario.containsKey(pLlave.toLowerCase()))
+            valor = diccionario.get(pLlave.toLowerCase());
+        
+        return valor;
+    }
+    
+    public void eliminarLlave(String pLlave){
+        diccionario.remove(pLlave.toLowerCase());
+    }
+    
+    @Override
+    public String toString(){
+        String strDiccionario = "";
+        
+        for (Map.Entry<String, Object> dato : diccionario.entrySet()){
+            strDiccionario = strDiccionario + " Llave : " + dato.getKey() + " - Valor : " + dato.getValue() + "\n";
+        }
+        
+        return strDiccionario;
+    }
+
+    
+}
