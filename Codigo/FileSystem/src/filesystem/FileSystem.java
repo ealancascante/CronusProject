4a23d2162295b61f6d4188375f253ac881ef9951
Eliécer Alán <eliecer.alanc@gmail.com>
2016-06-18 20:05:21 -0600

    Imprime arbol recursivo. Falta hacer bonito el print.

--- Codigo/FileSystem/src/filesystem/FileSystem.java
+++ Codigo/FileSystem/src/filesystem/FileSystem.java
@@ -13,44 +13,19 @@ import java.util.logging.Logger;
 public class FileSystem {
 
     public static void main(String[] args) {
-       /* Arbol arbol = new Arbol();
+        Arbol arbol = new Arbol();
         
         arbol.agregarElemento(new Directorio("Elem11"));       
         arbol.agregarElemento(new Directorio("Elem12"));
         arbol.agregarElemento(new ArchivoTexto("Elem13","doc","Adios",45));
+        arbol.agregarElemento(new ArchivoTexto("Alex.doc","doc","Adios",45));
+        
+        String[] a = {"R:", "Elem11"};
+        arbol.recorrer(a , "REAL");
+       arbol.agregarElemento(new Directorio("Alex 2"));
         //System.out.println(arbol.getNodoActual());
         
-        String newArray1[] = {"Elem11"};
-        System.out.println(arbol.recorrer(newArray1));
-        //System.out.println("Nodo Actual: "+arbol.getNodoActual());
         
-        arbol.agregarElemento(new ArchivoTexto("Elem21","doc","Adios",35));
-        arbol.agregarElemento(new ArchivoTexto("Elem22","doc","Adios",30));
-        
-        
-        System.out.println();
-        
-        String newArray2[] = {"raiz","Elem12"};
-        System.out.println(arbol.recorrer(newArray2));
-        //System.out.println("Nodo Actual: "+arbol.getNodoActual());
-        
-        arbol.agregarElemento(new Directorio("Elem23"));
-        arbol.agregarElemento(new ArchivoTexto("Elem24","doc","Hola",50));
-        
-        String newArray3[] = {"Elem23"};
-        System.out.println("\n"+arbol.recorrer(newArray3)); 
-        
-        arbol.agregarElemento(new ArchivoTexto("Elem31","doc","Hola",25));
-        
-        
-        String newArray4[] = {"Elem12","raiz"};
-        System.out.println("\n"+arbol.recorrer(newArray4));
-        
-        System.out.println(arbol.mostrarEstructura());
-
-         
-        /*long i = arbol.obtenerNodo("Elem12").calcularTamanoElemento();
-        System.out.println(" long "+i);*/
        
        SistemaArchivo_Cronus sistem = new SistemaArchivo_Cronus();
        sistem.agregarArchivoTexto("Archivo1.jpg", 1, "Este es el archivo 1");
@@ -106,7 +81,7 @@ public class FileSystem {
        System.out.println("Copiando \n"+sistem.moverElemento("Archivo1.jpg", "Directorio1\\Directorio3"));
        System.out.println(sistem.mostrarArbol());
        
-       
+      arbol.printFileSystem();
        
        
     }
